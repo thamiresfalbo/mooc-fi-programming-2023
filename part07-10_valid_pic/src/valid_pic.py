8e7ae9b1f029691fa18bd2987719b63a18ab724f
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-08-03 20:49:23 -0300

    Qui  3 Ago 2023 20:49:23 -03

--- part07-10_valid_pic/src/valid_pic.py
+++ part07-10_valid_pic/src/valid_pic.py
@@ -1,2 +1,42 @@
 # Write your solution here
 from datetime import datetime, timedelta
+
+
+def is_it_valid(pic: str) -> bool:
+    CONTROL_STRING = "0123456789ABCDEFHJKLMNPRSTUVWXY"
+    CENTURY_DICT = {"+": 1800, "-": 1900, "A": 2000}
+
+    # Check pic length
+    if len(pic) > 11:
+        return False
+
+    # The format is ddmmyyXyyyZ
+    day = int(pic[0:2])
+    month = int(pic[2:4])
+    year = int(pic[4:6])
+    century = pic[6]
+    control_character = pic[10]
+
+    try:
+        numbers = int(pic[0:6] + pic[7:10]) % 31
+        # Check century marker and control character
+        if (
+            century not in CENTURY_DICT.keys()
+            or CONTROL_STRING[numbers] != control_character
+        ):
+            return False
+
+        year += CENTURY_DICT[century]
+
+        # Check date is valid
+        datetime(year, month, day)
+
+        return True
+    except:
+        return False
+
+
+if __name__ == "__main__":
+    print(is_it_valid("230827-906F"))
+    print(is_it_valid("120488+246L"))
+    print(is_it_valid("310823A9877"))
