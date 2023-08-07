6bd9546c225e5c8f96b6759d08cc91a67ed458df
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-08-07 18:40:09 -0300

    .

--- part08-14_clock/src/clock.py
+++ part08-14_clock/src/clock.py
@@ -1,1 +1,45 @@
 # Write your solution here:
+class Clock:
+    def __init__(self, hour: int, minutes: int, seconds: int) -> None:
+        self.hour = hour
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def tick(self):
+        if self.minutes == 59 and self.seconds == 59 and self.hour == 23:
+            self.hour = 0
+            self.minutes = 0
+            self.seconds = 0
+        elif self.seconds == 59:
+            self.minutes += 1
+            self.seconds = 0
+        else:
+            self.seconds += 1
+
+    def set(self, hour: int, minutes: int):
+        self.hour = hour
+        self.minutes = minutes
+        self.seconds = 0
+
+    def __str__(self):
+        return f"{self.hour:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+
+if __name__ == "__main__":
+    clock = Clock(23, 59, 55)
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+
+    clock.set(12, 5)
+    print(clock)
--- part08-15_lunchcard/src/lunchcard.py
+++ part08-15_lunchcard/src/lunchcard.py
@@ -1,1 +1,49 @@
 # Write your solution here:
+class LunchCard:
+    def __init__(self, balance: float):
+        self.balance = balance
+
+    def __str__(self):
+        return f"The balance is {self.balance:.1f} euros"
+
+    def eat_lunch(self):
+        if self.balance >= 2.60:
+            self.balance -= 2.60
+
+    def eat_special(self):
+        if self.balance >= 4.60:
+            self.balance -= 4.60
+
+    def deposit_money(self, amount: float):
+        if amount < 0:
+            raise ValueError
+        self.balance += amount
+
+
+peters_card = LunchCard(20)
+graces_card = LunchCard(30)
+# the rest of your main function
+peters_card.eat_special()
+graces_card.eat_lunch()
+
+print("Peter: ", end="")
+print(peters_card)
+print("Grace: ", end="")
+print(graces_card)
+
+peters_card.deposit_money(20)
+graces_card.eat_special()
+
+print("Peter: ", end="")
+print(peters_card)
+print("Grace: ", end="")
+print(graces_card)
+
+peters_card.eat_lunch()
+peters_card.eat_lunch()
+graces_card.deposit_money(50)
+
+print("Peter: ", end="")
+print(peters_card)
+print("Grace: ", end="")
+print(graces_card)
