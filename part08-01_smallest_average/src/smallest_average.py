e3d198ccad9220b30deb0055f3af2ee2d9140bd1
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-08-07 15:02:31 -0300

    Mon Aug  7 15:02:30 -03 2023

--- part08-01_smallest_average/src/smallest_average.py
+++ part08-01_smallest_average/src/smallest_average.py
@@ -1,1 +1,23 @@
 # Write your solution here
+def smallest_average(person1: dict, person2: dict, person3: dict):
+    persons = [person1, person2, person3]
+    average = []
+
+    for i in range(len(persons)):
+        b = persons[i]["result1"] + persons[i]["result2"] + persons[i]["result3"] / 2
+        average.append(b)
+
+    average.sort()
+
+    for i in range(len(persons)):
+        c = persons[i]["result1"] + persons[i]["result2"] + persons[i]["result3"] / 2
+        if average[0] == c:
+            return persons[i]
+
+
+if __name__ == "__main__":
+    person1 = {"name": "Mary", "result1": 2, "result2": 3, "result3": 3}
+    person2 = {"name": "Gary", "result1": 5, "result2": 1, "result3": 8}
+    person3 = {"name": "Larry", "result1": 3, "result2": 1, "result3": 1}
+
+    print(smallest_average(person1, person2, person3))
--- part08-04_shopping_list/src/shopping_list.py
+++ part08-04_shopping_list/src/shopping_list.py
@@ -16,6 +16,22 @@ class ShoppingList:
     def amount(self, n: int):
         return self.products[n - 1][1]
 
+
 # -------------------------
 # Write your solution here:
 # -------------------------
+def total_units(my_list: ShoppingList):
+    total = 0
+    for i in range(1, my_list.number_of_items() + 1):
+        total += my_list.amount(i)
+
+    return total
+
+
+if __name__ == "__main__":
+    my_list = ShoppingList()
+    my_list.add("bananas", 10)
+    my_list.add("apples", 5)
+    my_list.add("pineapple", 1)
+
+    print(total_units(my_list))
