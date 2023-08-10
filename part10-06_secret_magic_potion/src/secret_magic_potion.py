04f3aa0f64dfa1d29ff29c83a5ed0d05a8c45b6c
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-08-09 21:02:49 -0300

    Wed Aug  9 21:02:49 -03 2023

--- part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -13,3 +13,34 @@ class MagicPotion:
             print(f"{ingredient[0]} {ingredient[1]} grams")
 
 
+# If the password argument given to either of these methods is wrong,
+# the methods should raise a ValueError exception.
+# If the password is correct, each method should call the relevant
+# method in the parent class. Do not copy and paste anything from the MagicPotion class.
+
+
+# An example of how this would work:
+class SecretMagicPotion(MagicPotion):
+    def __init__(self, name: str, password: str):
+        super().__init__(name)
+        self._password = password
+
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if password != self._password:
+            raise ValueError
+        super().add_ingredient(ingredient, amount)
+
+    def print_recipe(self, password: str):
+        if password != self._password:
+            raise ValueError
+        super().print_recipe()
+
+
+if __name__ == "__main__":
+    diminuendo = SecretMagicPotion("Diminuendo maximus", "hocuspocus")
+    diminuendo.add_ingredient("Toadstool", 1.5, "hocuspocus")
+    diminuendo.add_ingredient("Magic sand", 3.0, "hocuspocus")
+    diminuendo.add_ingredient("Frogspawn", 4.0, "hocuspocus")
+    diminuendo.print_recipe("hocuspocus")
+
+    diminuendo.print_recipe("pocushocus")  # WRONG password!
--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -1,8 +1,34 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self.__euros = euros
+        self.__cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        money = f"{float(self.__euros + (self.__cents / 100)):.2f}"
+        return f"{money} eur"
+
+    def __eq__(self, another) -> bool:
+        return self.__str__() == another.__str__()
+
+    def __lt__(self, another) -> bool:
+        return self.__str__() < another.__str__()
+
+    def __gt__(self, another) -> bool:
+        return self.__str__() > another.__str__()
+
+    def __ne__(self, another) -> bool:
+        return self.__str__() != another.__str__()
+
+    def __add__(self, another):
+        a = self.__euros + another.__euros
+        b = self.__cents + another.__cents
+        return Money(a, b)
+
+    def __sub__(self, another):
+        a = self.__euros - another.__euros
+        b = self.__cents - another.__cents
+        z = float(a + (b / 100))
+        if z < 0:
+            raise ValueError
+        return Money(a, b)
