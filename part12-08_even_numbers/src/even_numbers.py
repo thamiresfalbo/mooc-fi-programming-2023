51b6fa3dbbddc778bb78b98dccff4820645d732c
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-08-30 17:43:05 -0300

    Finished

--- part12-08_even_numbers/src/even_numbers.py
+++ part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def even_numbers(beginning: int, maximum: int):
+    num = beginning
+    while num <= maximum:
+        if num % 2 == 0:
+            yield num
+        num += 1
+
+
+if __name__ == "__main__":
+    numbers = even_numbers(2, 10)
+    for number in numbers:
+        print(number)
--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def prime_numbers():
+    num = 2
+    while True:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            yield num
+        num += 1
+
+
+if __name__ == "__main__":
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
